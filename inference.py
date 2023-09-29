bc25133c50a1c09ac13231acfad0ee7d92f01a40
beingmechon <52150592+beingmechon@users.noreply.github.com>
2023-09-29 09:50:58 +0530

    Create inference.py

---
+++ inference.py
@@ -0,0 +1,66 @@
+from helper import *
+
+SAMPLER_MAP = {
+    "DPM++ Karras SDE": lambda config: DPMSolverMultistepScheduler.from_config(config, use_karras=True, algorithm_type="sde-dpmsolver++"),
+    "Euler": lambda config: EulerDiscreteScheduler.from_config(config),
+}
+
+def inference(
+    control_image: Image.Image,
+    prompt: str,
+    negative_prompt: str,
+    guidance_scale: float = 8.0,
+    controlnet_conditioning_scale: float = 1,
+    control_guidance_start: float = 1,
+    control_guidance_end: float = 1,
+    upscaler_strength: float = 0.5,
+    seed: int = -1,
+    sampler = "DPM++ Karras SDE"
+):
+    start_time = time.time()
+    start_time_struct = time.localtime(start_time)
+    start_time_formatted = time.strftime("%H:%M:%S", start_time_struct)
+    print(f"Inference started at {start_time_formatted}")
+
+    # Generate the initial image
+    #init_image = init_pipe(prompt).images[0]
+
+    # Rest of your existing code
+    control_image_small = center_crop_resize(control_image)
+    control_image_large = center_crop_resize(control_image, (1024, 1024))
+
+    main_pipe.scheduler = SAMPLER_MAP[sampler](main_pipe.scheduler.config)
+    my_seed = random.randint(0, 2**32 - 1) if seed == -1 else seed
+    generator = torch.Generator(device="cuda").manual_seed(my_seed)
+
+    out = main_pipe(
+        prompt=prompt,
+        negative_prompt=negative_prompt,
+        image=control_image_small,
+        guidance_scale=float(guidance_scale),
+        controlnet_conditioning_scale=float(controlnet_conditioning_scale),
+        generator=generator,
+        control_guidance_start=float(control_guidance_start),
+        control_guidance_end=float(control_guidance_end),
+        num_inference_steps=15,
+        output_type="latent"
+    )
+    upscaled_latents = upscale(out, "nearest-exact", 2)
+    out_image = image_pipe(
+        prompt=prompt,
+        negative_prompt=negative_prompt,
+        control_image=control_image_large,
+        image=upscaled_latents,
+        guidance_scale=float(guidance_scale),
+        generator=generator,
+        num_inference_steps=20,
+        strength=upscaler_strength,
+        control_guidance_start=float(control_guidance_start),
+        control_guidance_end=float(control_guidance_end),
+        controlnet_conditioning_scale=float(controlnet_conditioning_scale)
+    )
+    end_time = time.time()
+    end_time_struct = time.localtime(end_time)
+    end_time_formatted = time.strftime("%H:%M:%S", end_time_struct)
+    print(f"Inference ended at {end_time_formatted}, taking {end_time-start_time}s")
+    return out_image["images"][0]

12f9227eb34dd857e8df7f3a12484b7bae7485cc
beingmechon <52150592+beingmechon@users.noreply.github.com>
2023-09-29 18:12:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,8 @@
 from inference import inference
 from PIL import Image
-from diffusers import (
-    DiffusionPipeline,
-    AutoencoderKL,
-    StableDiffusionControlNetPipeline,
-    ControlNetModel,
-    StableDiffusionLatentUpscalePipeline,
-    StableDiffusionImg2ImgPipeline,
-    StableDiffusionControlNetImg2ImgPipeline,
-    DPMSolverMultistepScheduler,  # <-- Added import
-    EulerDiscreteScheduler  # <-- Added import
-)
-import time
 
-BASE_MODEL = "SG161222/Realistic_Vision_V5.1_noVAE"
 
-vae = AutoencoderKL.from_pretrained("stabilityai/sd-vae-ft-mse", torch_dtype=torch.float32)
-controlnet = ControlNetModel.from_pretrained("monster-labs/control_v1p_sd15_qrcode_monster", torch_dtype=torch.float32)#, torch_dtype=torch.float32)
-main_pipe = StableDiffusionControlNetPipeline.from_pretrained(
-    BASE_MODEL,
-    controlnet=controlnet,
-    vae=vae,
-    safety_checker=None,
-    torch_dtype=torch.float32,
-).to("cuda")
-
-image_pipe = StableDiffusionControlNetImg2ImgPipeline(**main_pipe.components, requires_safety_checker=False)
-
-control_image_path = ""
+control_image_path = "pattern.png"
 prompt = "Beautiful city"
 negative_prompt = "monochrome, lowres, worst quality, low quality"
 
@@ -38,7 +13,7 @@ control_guidance_start = 0.5
 control_guidance_end = 0.8
 upscaler_strength = 1
 seed = 123
-sampler = "DPM++ Karras SDE"
+# sampler = "DPM++ Karras SDE"
 sampler = "Euler"
 
 result = inference(control_image,

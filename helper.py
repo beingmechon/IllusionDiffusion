be0c107b41ce152f24cf26549c4cb22653ad48e1
beingmechon <52150592+beingmechon@users.noreply.github.com>
2023-09-29 09:48:42 +0530

    Create helper.py

---
+++ helper.py
@@ -0,0 +1,54 @@
+def center_crop_resize(img, output_size=(512, 512)):
+    width, height = img.size
+
+    # Calculate dimensions to crop to the center
+    new_dimension = min(width, height)
+    left = (width - new_dimension)/2
+    top = (height - new_dimension)/2
+    right = (width + new_dimension)/2
+    bottom = (height + new_dimension)/2
+
+    # Crop and resize
+    img = img.crop((left, top,  right, bottom))
+    img = img.resize(output_size)
+
+    return img
+
+def common_upscale(samples, width, height, upscale_method, crop=False):
+        if crop == "center":
+            old_width = samples.shape[3]
+            old_height = samples.shape[2]
+            old_aspect = old_width / old_height
+            new_aspect = width / height
+            x = 0
+            y = 0
+            if old_aspect > new_aspect:
+                x = round((old_width - old_width * (new_aspect / old_aspect)) / 2)
+            elif old_aspect < new_aspect:
+                y = round((old_height - old_height * (old_aspect / new_aspect)) / 2)
+            s = samples[:,:,y:old_height-y,x:old_width-x]
+        else:
+            s = samples
+
+        return torch.nn.functional.interpolate(s, size=(height, width), mode=upscale_method)
+
+def upscale(samples, upscale_method, scale_by):
+        #s = samples.copy()
+        width = round(samples["images"].shape[3] * scale_by)
+        height = round(samples["images"].shape[2] * scale_by)
+        s = common_upscale(samples["images"], width, height, upscale_method, "disabled")
+        return (s)
+
+def check_inputs(prompt: str, control_image: Image.Image):
+    if control_image is None:
+        raise gr.Error("Please select or upload an Input Illusion")
+    if prompt is None or prompt == "":
+        raise gr.Error("Prompt is required")
+
+def convert_to_pil(base64_image):
+    pil_image = processing_utils.decode_base64_to_image(base64_image)
+    return pil_image
+
+def convert_to_base64(pil_image):
+    base64_image = processing_utils.encode_pil_to_base64(pil_image)
+    return base64_image
